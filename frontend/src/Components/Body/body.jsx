0a5c7cbec2f7c157904d4e8012779236c8eca500
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2024-06-14 18:37:18 -0300

    Sprint terminado

--- frontend/src/Components/Body/body.jsx
+++ frontend/src/Components/Body/body.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import './body.css';
-import Slider from './slider';
-import Calendar from './calendar';
-import SocialFooter from './SocialFooter';
 
-function App() {
+function body() {
   return (
     <div className="App">
       <header className="App-header">
         <nav>
           <a href="#home">Inicio</a>
-          <a href="#news">Noticias</a>
           <a href="#schedule">Calendario</a>
           <a href="#teams">Equipos</a>
           <a href="#drivers">Pilotos</a>
-          <a href="#contact">Contacto</a>
         </nav>
       </header>
-      <Slider />
       <div className="content">
         <button className="view-drivers" onClick={() => window.location.href = '/drivers'}>Ver Pilotos</button>
       </div>
-      <Calendar />
-      <SocialFooter />
     </div>
   );
 }
 
-export default App;
+export default body;

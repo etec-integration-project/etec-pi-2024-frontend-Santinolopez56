c1c93250087c0560273eb6a55b689898db9d020e
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2024-11-28 19:28:19 -0300

    cart

--- frontend/src/Components/carrito/carrito.jsx
+++ frontend/src/Components/carrito/carrito.jsx
@@ -52,7 +52,7 @@ function Carrito() {
     function realizarCompra() {
         const carrito = localStorage.getItem('productos');
         console.log("Compra realizada con el carrito:", carrito);
-        axios.post("/app/realizarCompra",{
+        axios.post("/realizar-compra",{
             cart:JSON.stringify(localStorage.getItem("productos"))
         })
     }
